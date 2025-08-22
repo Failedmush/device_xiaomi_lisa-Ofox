b746ad7b733497e9c7f35a3650b0f08618665233
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-22 12:51:24 +0100

    update for lisa

--- twrp_lisa.mk
+++ twrp_lisa.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2021 The TWRP Open Source Project
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,22 +14,17 @@
 # limitations under the License.
 #
 
-DEVICE_PATH=device/xiaomi/lisa
+DEVICE_PATH := device/xiaomi/lisa
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-# Inherit from taoyao device
-$(call inherit-product, device/xiaomi/lisa/device.mk)
+# Inherit from device.mk configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := lisa
 
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lisa
 PRODUCT_NAME := twrp_lisa
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G NE
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-#
